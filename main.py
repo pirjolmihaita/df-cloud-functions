e0e746359faa0063566e4eae725a1d3eeecd30b6
pirjolmihaita <44243333+pirjolmihaita@users.noreply.github.com>
2025-05-10 16:45:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import math
 from google.cloud import firestore
 import numpy as np
-
+from flask import jsonify
 # Distribuție Laplace
 def adauga_zgomot_laplace(valoare_reală, epsilon):
     print(".,")
@@ -32,3 +32,13 @@ def adauga_document(request):
 
     return f"Adăugat {nume} cu zgomot: {valoare_cu_zgomot}", 200
 
+@functions_framework.http
+def obtine_documente(request):
+    db = firestore.Client()
+    rezultate = db.collection("persoane").stream()
+
+    lista = []
+    for doc in rezultate:
+        lista.append(doc.to_dict())
+
+    return jsonify(lista), 200
